53c63da8c405b0088be1dfd81344e9178f7a7c19
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-05 11:35:14 -0400

    created function saveToPersistantStore URL in BookController

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -24,4 +24,24 @@ class BookController {
              // /Apps/Stars/Documents/stars.plist
              return documents.appendingPathComponent("ReadingList.plist")
     }
+    
+    /*
+     Create a function called saveToPersistentStore(). This function will be responsible for saving any changes to any Book object so that the changes will still be there when the user comes back into the application. You can implement this by doing the following:
+
+     Create an instance of PropertyListEncoder.
+     Inside of a do-try-catch block create a constant called booksData. Using the encode(value: ...) function of the property list encoder, encode the books array into Data.
+     Call the write(to: URL) function on the data you encoded computed property. The url you pass in should be an unwrapped version of the readingListURL property.
+     */
+    
+    func saveToPersistentStore() {
+        guard let url = readingListURL else { return }
+        
+        do {
+        let encoder = PropertyListEncoder()
+        let booksdata = try encoder.encode(book)
+            try booksdata.write(to: url)
+        } catch {
+            print("Error saving stars data: \(error)")
+        }
+    }
 }
