e9100dd236d8e16e74f9447c1e85bad39ca21e42
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-06 16:36:21 -0400

    created UI, outlets and action buttons and detail view controller UI

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by denis cedeno on 10/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookLabel: UILabel!
+    
+    @IBOutlet weak var checkBoxButton: UIButton!
+    
+    @IBAction func checkBoxAction(_ sender: Any) {
+    }
+}
